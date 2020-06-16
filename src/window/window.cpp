0e34ea80ccca06ea0e7b7e0fba988c3c73cf0148
TegMhomas <gruszka.patryk@gmail.com>
2020-06-17 01:51:19 +0200

    grafiki+kod poprawka

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -1,5 +1,7 @@
 #include "window.hpp"
+#include <filesystem>
 #include <spdlog/spdlog.h>
+#include <string>
 
 Window::Window() {
   SPDLOG_INFO("constructing the window");
@@ -7,13 +9,13 @@ Window::Window() {
   m_window.create(sf::VideoMode(800, 600), "SUPREME-OCTO-RPG");
   m_window.setFramerateLimit(60);
 
-  if (!m_font.loadFromFile("resources/fonts/SourceCodePro-Regular.ttf")) {
+  if (!(m_font.loadFromFile("resources/fonts/SourceCodePro-Regular.ttf") ||
+        m_font.loadFromFile("../../../resources/fonts/SourceCodePro-Regular.ttf"))) {
     SPDLOG_CRITICAL("!m_font.loadFromFile()");
   }
 
   SPDLOG_INFO("window constructed nicely, all resources loaded");
-  SPDLOG_DEBUG("window constructed nicely, all resources loaded");
-  SPDLOG_TRACE("window constructed nicely, all resources loaded");
+  SPDLOG_INFO("current path: " + std::filesystem::current_path().string());
 }
 
 Window::~Window() {}
