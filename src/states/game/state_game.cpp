ff1e715f936a15d937aba092975e5250a2c7cdf9
Dreamykass <grunerdamian@gmail.com>
2020-06-17 13:23:30 +0200

    start

---
+++ src/states/game/state_game.cpp
@@ -0,0 +1,12 @@
+
+#include "states/state_game.hpp"
+#include "stack_machine.hpp"
+#include "utils.hpp"
+
+StateGame::StateGame() {
+  SPDLOG_INFO("state game constructing");
+}
+
+StateGame::~StateGame() {
+  SPDLOG_INFO("state game destructing");
+}
