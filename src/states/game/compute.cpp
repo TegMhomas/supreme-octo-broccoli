7deb3c16e9df708ac03bb3e7d3f2fb285a2c5b14
TegMhomas <gruszka.patryk@gmail.com>
2023-11-09 01:07:11 +0100

    Really smooth final player movement

--- src/states/game/compute.cpp
+++ src/states/game/compute.cpp
@@ -6,36 +6,33 @@
 void StateGame::Compute(StackMachine& _stack_machine) {
   if (m_done) {
     _stack_machine.m_game_stack.pop();
-    return; 
+    return;
   }
 
   sf::Time deltaTime = m_frame_clock.restart();
 
-  std::optional<std::reference_wrapper<Entity>> controllable;
+
   for (auto& entity : m_entities) {
     if (entity.m_type == Entity::Type::Player) {
-      controllable = std::ref(entity);
-      break; 
+
+      entity.m_acceleration = sf::Vector2f(0.f, 0.f);
+
+      if (m_key_map[sf::Keyboard::Up]) {
+        entity.m_acceleration.y -= Entity::accelerationRate;
+      }
+      if (m_key_map[sf::Keyboard::Down]) {
+        entity.m_acceleration.y += Entity::accelerationRate;
+      }
+      if (m_key_map[sf::Keyboard::Left]) {
+        entity.m_acceleration.x -= Entity::accelerationRate;
+      }
+      if (m_key_map[sf::Keyboard::Right]) {
+        entity.m_acceleration.x += Entity::accelerationRate;
+      }
+
+      entity.update(deltaTime);
     }
   }
 
-  if (controllable) {
-    Entity& entity = controllable->get();
-    float moveSpeed = 5.0f; 
-
-    if (m_key_map[sf::Keyboard::Up]) {
-      entity.m_position.y -= moveSpeed * deltaTime.asSeconds();
-    }
-    if (m_key_map[sf::Keyboard::Down]) {
-      entity.m_position.y += moveSpeed * deltaTime.asSeconds();
-    }
-    if (m_key_map[sf::Keyboard::Left]) {
-      entity.m_position.x -= moveSpeed * deltaTime.asSeconds();
-    }
-    if (m_key_map[sf::Keyboard::Right]) {
-      entity.m_position.x += moveSpeed * deltaTime.asSeconds();
-    }
-    // player collision...
-  }
 
 }
--- src/states/game/get_events.cpp
+++ src/states/game/get_events.cpp
@@ -9,39 +9,9 @@ void StateGame::GetEvents(const std::vector<sf::Event>& _events) {
     if (event.type == sf::Event::Closed) {
       m_done = true;
     } else if (event.type == sf::Event::KeyPressed) {
-      switch (event.key.code) {
-        case sf::Keyboard::Up:
-          m_key_map[sf::Keyboard::Up] = true;
-          break;
-        case sf::Keyboard::Down:
-          m_key_map[sf::Keyboard::Down] = true;
-          break;
-        case sf::Keyboard::Left:
-          m_key_map[sf::Keyboard::Left] = true;
-          break;
-        case sf::Keyboard::Right:
-          m_key_map[sf::Keyboard::Right] = true;
-          break;
-        default:
-          break; 
-      }
+      m_key_map[event.key.code] = true;
     } else if (event.type == sf::Event::KeyReleased) {
-      switch (event.key.code) {
-        case sf::Keyboard::Up:
-          m_key_map[sf::Keyboard::Up] = false;
-          break;
-        case sf::Keyboard::Down:
-          m_key_map[sf::Keyboard::Down] = false;
-          break;
-        case sf::Keyboard::Left:
-          m_key_map[sf::Keyboard::Left] = false;
-          break;
-        case sf::Keyboard::Right:
-          m_key_map[sf::Keyboard::Right] = false;
-          break;
-        default:
-          break; 
-      }
+      m_key_map[event.key.code] = false;
     }
   }
 }
